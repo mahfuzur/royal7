4f889f63ddb6f0cf16e48ee44f82e6f6834ff36d
Mahfuzur Rahman <limon3640@gmail.com>
2020-06-11 01:33:49 +0600

    add post module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostModule } from './post/post.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'post', component: PostModule }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
